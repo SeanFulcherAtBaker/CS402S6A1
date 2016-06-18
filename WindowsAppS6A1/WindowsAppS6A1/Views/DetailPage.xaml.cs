826a6d8259b47bcde0fbca19069c65cf449119d6
Sean Fulcher <sfulch01@baker.edu>
2016-06-18 12:58:06 -0400

    FinishedVid1

---
+++ WindowsAppS6A1/WindowsAppS6A1/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using WindowsAppS6A1.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsAppS6A1.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ WindowsAppS6A1/WindowsAppS6A1/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using WindowsAppS6A1.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace WindowsAppS6A1.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
