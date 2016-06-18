4b63fd592af03e32b7a6a2d43fcf5b868b0499fb
Sean Fulcher <sfulch01@baker.edu>
2016-06-18 13:35:45 -0400

    Vid2 Finished

---
+++ WindowsApp1Vid2/WindowsApp1Vid2/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using WindowsApp1Vid2.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsApp1Vid2.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
