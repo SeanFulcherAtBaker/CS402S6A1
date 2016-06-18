0d0041d073dfc48c92826caf7db671fed38ed829
Sean Fulcher <sfulch01@baker.edu>
2016-06-18 14:23:49 -0400

    Finished Vid4

---
+++ WindowsAppVid4/WindowsAppVid4/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using WindowsAppVid4.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace WindowsAppVid4.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
